4527bd3a9194bfe66887c4ada82582bc5895f97e
Anna Harris <jharris@camphillschool.org>
2020-01-28 17:48:33 -0500

    Added all void functions and some initial constants and declared variables

--- Source.cpp
+++ Source.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
+const int width = 50;
+const int height = 50;
+bool gameOver;
+int x, y, fruitX, fruitY;
+
+
+
 void setup()
 {
 
 }
 
+void draw()
+{
 
+}
+
+void input()
+{
+
+}
+
+void logic()
+{
+
+}
